48dcf571fd5fa3ed3f1121f45d4ee17b57b29b17
Gabriele31201 <gabri31201@gmail.com>
2023-04-12 15:44:21 +0200

    add all date for the book

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.Write("------------------------ CLEAN CODE di Robert C.Martin ------------------------");
+﻿
+Console.WriteLine();
+
+    Console.Write("------------------------ CLEAN CODE di Robert C.Martin ------------------------");
+
+Console.WriteLine();
+
+    Console.Write("informazioni generiche");
+
+Console.WriteLine();
+
+    
+    long isbn = 9780132350884;    
+    int numberPage = 431;
+    float weightbook = 0.800f;
+    float lengthBook = 17.78f;
+    float thicknessBook = 2.79f;
+    float widthBook = 23.37f;
+
+Console.Write(" - " + "ISBN" + isbn);
+
+
+
+
